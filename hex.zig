9dcf7f0db54ea6bb8c20207b03a76c971d83deae
rishit khandelwal <rishitkh@gmail.com>
2024-06-18 23:58:17 +0530

    using slice instead of ArrayList in hopes of improving performance as compared to xxd; does not improve performance notably; TODO improve performance

--- hex.zig
+++ hex.zig
@@ -17,8 +17,7 @@ pub fn main() !void {
         var buf_reader = std.io.bufferedReader(file.reader());
         const reader = buf_reader.reader();
 
-        var buf = std.ArrayList(u8).init(allocator);
-        defer buf.deinit();
+        var buf: [4096]u8 = undefined;
 
         var line = std.ArrayList(u8).init(allocator);
         defer line.deinit();
@@ -27,35 +26,41 @@ pub fn main() !void {
             try std.fmt.parseInt(u8, args[2], 10)
         else
             16;
-        _ = try reader.readAllArrayList(&buf, 1 << 32);
 
-        for (buf.items, 0..) |byte, ind| {
-            if (ind % clms == 0) {
-                std.debug.print("{x:0>8}: ", .{ind / clms});
-            }
+        var bytes = try reader.readAll(&buf);
+        var ind: u32 = 0;
+        while (bytes != 0) {
+            for (buf) |byte| {
+                defer ind += 1;
+                if (ind % clms == 0) {
+                    std.debug.print("{x:0>8}: ", .{ind / clms});
+                }
 
-            if (ind % 2 == 0) {
-                std.debug.print(" ", .{});
-            }
+                if (ind % 2 == 0) {
+                    std.debug.print(" ", .{});
+                }
 
-            try stdout.print("{x:0>2}", .{byte});
-            if (byte != '\n') {
-                try line.append(byte);
-            } else {
-                try line.appendSlice(" ");
-            }
+                try stdout.print("{x:0>2}", .{byte});
 
-            if (ind % clms == clms - 1) {
-                std.debug.print("  {s}\n", .{line.items});
-                line.clearAndFree();
+                if (' ' <= byte and byte <= '~') {
+                    try line.append(byte);
+                } else {
+                    try line.append('.');
+                }
+
+                if (ind % clms == clms - 1) {
+                    std.debug.print("  {s}\n", .{line.items});
+                    line.clearAndFree();
+                }
             }
+            bytes = try reader.readAll(&buf);
         }
         const l = clms - line.items.len;
-        for (0..l) |_| {
+        for (0..l) |i| {
             std.debug.print("  ", .{});
-        }
-        for (0..(l / 2)) |_| {
-            std.debug.print(" ", .{});
+            if (i % 2 == 1) {
+                std.debug.print(" ", .{});
+            }
         }
 
         std.debug.print("  {s}\n", .{line.items});
