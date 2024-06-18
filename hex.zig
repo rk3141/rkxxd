8aec0597bfcf6987cbcfa5dc1431816d69245013
rishit khandelwal <rishitkh@gmail.com>
2024-06-18 21:12:10 +0530

    line numbering and printing charecters on the side; TODO filter non-printable chareceters

--- hex.zig
+++ hex.zig
@@ -20,6 +20,9 @@ pub fn main() !void {
         var buf = std.ArrayList(u8).init(allocator);
         defer buf.deinit();
 
+        var line = std.ArrayList(u8).init(allocator);
+        defer line.deinit();
+
         const clms = if (args.len > 2)
             try std.fmt.parseInt(u8, args[2], 10)
         else
@@ -36,10 +39,27 @@ pub fn main() !void {
             }
 
             try stdout.print("{x:0>2}", .{byte});
+            if (byte != '\n') {
+                try line.append(byte);
+            } else {
+                try line.appendSlice(" ");
+            }
+
             if (ind % clms == clms - 1) {
-                std.debug.print("\n", .{});
+                std.debug.print("  {s}\n", .{line.items});
+                line.clearAndFree();
             }
         }
+        const l = clms - line.items.len;
+        for (0..l) |_| {
+            std.debug.print("  ", .{});
+        }
+        for (0..(l / 2)) |_| {
+            std.debug.print(" ", .{});
+        }
+
+        std.debug.print("  {s}\n", .{line.items});
+        line.clearAndFree();
     } else {
         try stdout.print("rkxxd - github.com/rk3141", .{});
     }
